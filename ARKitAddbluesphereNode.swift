ee245c429693897a3255a39147615bc23f5e0c56
Zack Toth <zacharyrtoth@gmail.com>
2018-01-28 20:05:30 -0800

    Create ARKitAddbluesphereNode.swift

---
+++ ARKitAddbluesphereNode.swift
@@ -0,0 +1,36 @@
+import UIKit
+import SceneKit
+import ARKit
+
+class ViewController: UIViewController, ARSCNViewDelegate {
+
+    @IBOutlet var sceneView: ARSCNView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        sceneView.delegate = self
+        sceneView.showsStatistics = true
+        sceneView.scene = SCNScene()
+        let circleNode = createSphereNode(with: 0.2, color: .blue)
+        circleNode.position = SCNVector3(0, 0, -1) // 1 meter in front of camera 
+        sceneView.scene.rootNode.addChildNode(circleNode)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        let configuration = ARWorldTrackingConfiguration()
+        sceneView.session.run(configuration)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        sceneView.session.pause()
+    }
+    
+    func createSphereNode(with radius: CGFloat, color: UIColor) -> SCNNode {
+        let geometry = SCNSphere(radius: radius)
+        geometry.firstMaterial?.diffuse.contents = color
+        let sphereNode = SCNNode(geometry: geometry)
+        return sphereNode
+    }
+}
